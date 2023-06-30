7f0712b1211e8fb10c80e3723bacf7603676c7b1
quochuynh <quochuynh@lecle.co.kr>
2023-06-30 11:10:44 +0700

    remove others platform

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import fvp
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FvpPlugin.register(with: registry.registrar(forPlugin: "FvpPlugin"))
 }
